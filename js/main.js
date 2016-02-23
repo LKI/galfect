4bde4e90058c605ae6c0bdbb20368fde5143c7ec
Lirian Su <liriansu@gmail.com>
2016-02-23 13:41:29 +0800

    Separate story and index.html
    * We can display choose now

---
+++ js/main.js
@@ -0,0 +1,60 @@
+var strs = [
+  {
+    dialog : "Magic Mirror in my hand,",
+    avatar : "avatar/queen.jpg",
+    scene  : "scene/castle.jpg",
+  },
+  {
+    dialog : "Who is the fairest in the land?",
+  },
+  {
+    dialog : "My Queen,",
+    avatar : "avatar/mirror.jpg",
+  },
+  {
+    dialog : "You are the fairest in the land.",
+  },
+  {
+    dialog : "When Snow White reaches the age of seven",
+    avatar : "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=",
+  },
+  {
+    dialog : "Magic Mirror in my hand,",
+    avatar : "avatar/queen.jpg",
+  },
+  {
+    dialog : "Who is the fairest in the land?",
+  },
+  {
+    dialog : "My Queen,",
+    avatar : "avatar/mirror.jpg",
+  },
+  {
+    dialog : "You are the fairest here so true.",
+  },
+  {
+    dialog : "But Snow White is a thousand times more beautiful than you.",
+  },
+  {
+    dialog : "Evil Queen became angry about this.",
+    avatar : "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=",
+  },
+  {
+    dialog : "She enlist a huntsman to kill Snow White.",
+  },
+  {
+    dialog : "My little bear,",
+    avatar : "avatar/white.jpg",
+    scene  : "scene/princess-room.jpg",
+  },
+  {
+    dialog : "Should we go outside to play?",
+  },
+  {
+    choose : { "Go outside" : 15, "Stay at home" : 15, },
+  },
+  {
+    avatar : "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=",
+    dialog : "To Be Continue",
+  }
+];
